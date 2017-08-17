673bebcf09cd7c2324db5c7a34edcc159f03ce7a
Michael Vogl <info@mv-web.net>
2017-08-17 12:41:32 +0200

    Setup initial project and structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
+import { PageNotFoundComponent } from './404.component';
+
+import { Game } from './games/game.component'
+import { NewGame } from './games/new.component';
+
+const appRoutes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'game', component: NewGame },
+  { path: 'game/:id', component: Game },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    PageNotFoundComponent,
+    Game,
+    NewGame
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/games/new.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './new.component.html'
+})
+export class NewGame {}
