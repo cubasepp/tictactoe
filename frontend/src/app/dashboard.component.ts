27959419fb0f94de647a64c615767657e0c4ae27
Michael Vogl <info@mv-web.net>
2017-09-11 14:20:48 +0200

    Add dashboard with game history

--- frontend/src/app/dashboard.component.ts
+++ frontend/src/app/dashboard.component.ts
@@ -13,9 +13,32 @@ export class DashboardComponent implements OnInit {
 
   games: Game[]
 
+  start: number = 0;
+  steps: number = 10;
+
   constructor(private gameService: GameService) {}
 
   ngOnInit(): void {
     this.gameService.getGames().then(games => { this.games = games });
+  }
+
+  checkWin(moves: Array<string>): boolean {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (moves[a] && moves[a] === moves[b] && moves[a] === moves[c]) {
+        return true;
+      }
+    }
+    return false;
   }
 }
