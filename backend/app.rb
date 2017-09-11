3de8009f0c264a106e50f52490d6e05e23a6e563
Michael Vogl <info@mv-web.net>
2017-09-11 10:53:52 +0200

    Fix all url paths

--- backend/app.rb
+++ backend/app.rb
@@ -10,7 +10,7 @@ before do
   content_type 'application/json'
 end
 
-get "/game" do
+get "/game/" do
   JSON games: Game.all.as_json(include: :players)
 end
 
@@ -18,7 +18,7 @@ get "/game/:id" do
   JSON Game.find_by_id(params[:id]).as_json(include: :players) || halt(404)
 end
 
-post "/game" do
+post "/game/" do
   game = Game.new.from_json(request.body.read)
   if game.save
      JSON game.as_json(include: :players)
