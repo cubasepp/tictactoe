584c8254283ad17fad93b8c29bea750275201eef
Michael Vogl <info@mv-web.net>
2017-09-11 16:05:56 +0200

    Add config stuff for run Puma on production mode

--- backend/app.rb
+++ backend/app.rb
@@ -6,32 +6,41 @@ require "./models/player.rb"
 
 set :database, "sqlite3:tictactoe.sqlite3"
 
-before do
-  content_type 'application/json'
-end
+class Api < Sinatra::Base
 
-get "/game/" do
-  JSON games: Game.all.as_json(include: :players)
-end
-
-get "/game/:id" do
-  JSON Game.find_by_id(params[:id]).as_json(include: :players) || halt(404)
-end
-
-post "/game/" do
-  game = Game.new.from_json(request.body.read)
-  if game.save
-     JSON game.as_json(include: :players)
-  else
-    halt(422)
+  before do
+    headers 'Access-Control-Allow-Origin' => ''
+    content_type 'application/json'
   end
-end
 
-post "/game/:id" do
-  game = Game.find_by_id(params[:id]) || halt(404)
-  if game.update(moves: JSON.parse(request.body.read))
-    JSON game.as_json(include: :players)
-  else
-    halt(422)
+  get "/game/" do
+    JSON games: Game.all.order(id: :desc).as_json(include: :players)
+  end
+
+  get "/game/:id" do
+    JSON Game.find_by_id(params[:id]).as_json(include: :players) || halt(404)
+  end
+
+  post "/game/" do
+    game = Game.new.from_json(request.body.read)
+    if game.save
+      JSON game.as_json(include: :players)
+    else
+      halt(422)
+    end
+  end
+
+  post "/game/:id" do
+    game = Game.find_by_id(params[:id]) || halt(404)
+    if game.update(moves: JSON.parse(request.body.read))
+      JSON game.as_json(include: :players)
+    else
+      halt(422)
+    end
+  end
+  
+   not_found do
+    status 404
+    redirect "https://tictactoe.voglm.de", "rong place, buddy"
   end
 end
